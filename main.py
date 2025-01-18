b91dd409f6db04fd455b67be0b36856e31f785e0
ihor <i0962112822@gmail.com>
2025-01-18 11:59:39 +0200

    init

---
+++ main.py
@@ -0,0 +1,89 @@
+
+from datetime import datetime, timedelta
+import random
+import re
+
+
+
+
+def get_days_from_today(date: str)-> int:
+    """
+    Calculate the difference between the given date and today date.
+
+    Parameters:
+    date (str): Date in the format 'YYYY-MM-DD'.
+
+    Returns:
+    int: Difference in days.
+    """
+    date_parts: list = date.split("-")
+    new_date: datetime = datetime(int(date_parts[0]), int(date_parts[1]), int(date_parts[2]) )
+    now_date: datetime = datetime.today()
+    result: timedelta = now_date - new_date
+    return result.days
+
+print(get_days_from_today('2020-10-09'))
+
+
+
+
+def get_numbers_ticket(min: int, max: int, quantity: int)-> list:
+    """
+    Return a list of random numbers in given range.
+
+    Parameters:
+    min (int): Lower bound of the range.
+    max (int): Upper bound of the range.
+    quantity (int): Quantity of random numbers to generate.
+
+    Returns:
+    list: List of random numbers.
+    """
+    return [random.randint(min, max) for _ in range(quantity)]
+
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
+
+
+
+def normalize_phone(phone_number: str)-> str:
+    """
+    Return a normalized phone number.
+
+    Parameters:
+    phone_number (str): Phone number to normalize.
+
+    Returns:
+    str: Normalized phone number.
+    """
+    only_digits = re.sub(r"\D", "", phone_number)
+    
+    if only_digits.startswith("380"):
+        only_digits = "+" + only_digits
+    elif only_digits.startswith("0"):
+        only_digits = "+38" + only_digits
+
+    return only_digits
+  
+    
+    
+
+
+
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+
+
+print(sanitized_numbers)
